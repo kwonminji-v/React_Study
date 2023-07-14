5b661e20c5f38ef848aab10ce26e38cc454c774a
kwonminji <minji6313@yahoo.com>
2023-07-14 12:26:15 +0900

    #1.React_복습

---
+++ codingapple-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+
+  let post = '백준 2문제씩 풀기';
+
+
+  return (
+    <div className="App">
+      <div className = "black-nav">
+        <h4>To-Do 만들어보기</h4>
+      </div>
+      <h4>{ post }</h4>
+    </div>
+  );
+}
+
+export default App;
