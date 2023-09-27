33ae34d5647331194f322ad92dc24242862adfda
kwonminji <minji6313@yahoo.com>
2023-09-27 14:34:36 +0900

    #3.React_State

--- codingapple-react/src/App.js
+++ codingapple-react/src/App.js
@@ -1,20 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let post = '백준 2문제씩 풀기';
+  let post = '백준 2문제씩 풀기'; //자료 잠깐 저장 시 변수 사용
+  let [todo , setTodo] = useState("리액트 인강 10개 듣기" , "백준 문제 2개씩 풀기");
 
-
+  let num = [1,2];
+ 
   return (
     <div className="App">
       <div className = "black-nav">
         <h4>To-Do 만들어보기</h4>
       </div>
       <div className="list">
-        <h4>글 제목</h4>
+        <h4>{todo}</h4>
         <p>작성일자</p>
       </div>
-      <h4>{ post }</h4>
     </div>
   );
 }
