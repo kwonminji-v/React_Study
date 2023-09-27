d83e0a5beb0d57d29e6772305585bfa3d141d2a2
kwonminji <minji6313@yahoo.com>
2023-09-27 12:26:15 +0900

    #2.React_JSX작성

--- codingapple-react/src/App.js
+++ codingapple-react/src/App.js
@@ -10,6 +10,10 @@ function App() {
       <div className = "black-nav">
         <h4>To-Do 만들어보기</h4>
       </div>
+      <div className="list">
+        <h4>글 제목</h4>
+        <p>작성일자</p>
+      </div>
       <h4>{ post }</h4>
     </div>
   );
