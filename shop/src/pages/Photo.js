143d75e9eb5d2a3a2e377bebd53e3adcfeadf48b
kwonminji <minji6313@yahoo.com>
2023-10-16 17:05:16 +0900

    #지난 과정 복습_1

--- shop/src/pages/Photo.js
+++ shop/src/pages/Photo.js
@@ -1,27 +1,43 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
 
-   
+
+let YelloBtn = styled.button`
+  background : yellow;
+  color : black;
+  padding : 10px;  
+`
+
+let BlackBox = styled.div`
+  background : black;
+  padding : 20px;
+  color : white;
+
+`
 
 function Photo(props) {
 
   const { id } = useParams();
 
-  let 찾은상품 = props.datas.find((data) => {
-    return data.id == id;
-  });
+  const photos = props.datas.find((array)=> {return array.id == id})
+  console.log(photos);
 
     return (
         <>
         <div style={{textAlign:"center",height:"40px",backgroundColor:"skyblue", marginBottom:"35px"}}>메인페이지에 게시된 사진들 보여주는 페이지</div>
         <div className="container">
+
+          <BlackBox>ㅋㄷㅋㄷㅋㄷㅋㄷㅋㄷㅋㄷ</BlackBox>
+          <YelloBtn>와라라라라라ㅏ</YelloBtn>
+
           <div className="row">
             <div className="col-md-6">
             <img src={process.env.PUBLIC_URL + '../img/pubao'+(id)+'.jpg'}  width="230px" alt="" style={{border:"1px solid red"}}/>
             </div>
             <div className="col-md-4">
-              <h4>사진 제목 : {찾은상품.title}</h4>
-              <p>사진 설명 : {찾은상품.content }</p>
-              <p>촬영 날짜 : {찾은상품.date}</p>
+              <h4>사진 제목 : {photos.title}</h4>
+              <p>사진 설명 : {photos.content }</p>
+              <p>촬영 날짜 : {photos.date}</p>
               <button className="btn btn-danger">댓글 남기기</button> 
             </div>
           </div>
